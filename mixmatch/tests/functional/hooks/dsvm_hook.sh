de20a7f4ca91c8cf2f75fc3feacad4488c9fcbe6
Kristi Nikolla <knikolla@bu.edu>
2016-11-11 12:40:42 -0500

    Add Devstack plugin for mixmatch
    This plugin automatically configures the mixmatch proxy, the config
    of nova/cinder, the endpoints and starts the proxy.
    
    To enable it add this to local.conf
    
    enable_plugin mixmatch git://git.openstack.org/openstack/mixmatch.git
    
    Change-Id: I1fd872288090378a5d91ff102d8d5054b14b9608

--- mixmatch/tests/functional/hooks/dsvm_hook.sh
+++ mixmatch/tests/functional/hooks/dsvm_hook.sh
@@ -14,4 +14,46 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+function get_endpoint_ids {
+    echo `openstack endpoint list --service $1 -c ID -f value`
+}
+
+function register_mixmatch {
+    # Update the endpoints
+    openstack endpoint delete `get_endpoint_ids image`
+    openstack endpoint delete `get_endpoint_ids volume`
+    openstack endpoint delete `get_endpoint_ids volumev2`
+    openstack endpoint delete `get_endpoint_ids volumev3`
+
+    get_or_create_endpoint \
+        "image" \
+        "$REGION_NAME" \
+        "$MIXMATCH_SERVICE_PROTOCOL://$HOST_IP:$MIXMATCH_SERVICE_PORT/image" \
+        "$MIXMATCH_SERVICE_PROTOCOL://$HOST_IP:$MIXMATCH_SERVICE_PORT/image" \
+        "$MIXMATCH_SERVICE_PROTOCOL://$HOST_IP:$MIXMATCH_SERVICE_PORT/image"
+
+    get_or_create_endpoint \
+        "volume" \
+        "$REGION_NAME" \
+        "$MIXMATCH_SERVICE_PROTOCOL://$HOST_IP:$MIXMATCH_SERVICE_PORT/volume/v1/\$(project_id)s" \
+        "$MIXMATCH_SERVICE_PROTOCOL://$HOST_IP:$MIXMATCH_SERVICE_PORT/volume/v1/\$(project_id)s" \
+        "$MIXMATCH_SERVICE_PROTOCOL://$HOST_IP:$MIXMATCH_SERVICE_PORT/volume/v1/\$(project_id)s"
+
+    get_or_create_endpoint \
+        "volumev2" \
+        "$REGION_NAME" \
+        "$MIXMATCH_SERVICE_PROTOCOL://$HOST_IP:$MIXMATCH_SERVICE_PORT/volume/v2/\$(project_id)s" \
+        "$MIXMATCH_SERVICE_PROTOCOL://$HOST_IP:$MIXMATCH_SERVICE_PORT/volume/v2/\$(project_id)s" \
+        "$MIXMATCH_SERVICE_PROTOCOL://$HOST_IP:$MIXMATCH_SERVICE_PORT/volume/v2/\$(project_id)s"
+
+    get_or_create_endpoint \
+        "volumev3" \
+        "$REGION_NAME" \
+        "$MIXMATCH_SERVICE_PROTOCOL://$HOST_IP:$MIXMATCH_SERVICE_PORT/volume/v3/\$(project_id)s" \
+        "$MIXMATCH_SERVICE_PROTOCOL://$HOST_IP:$MIXMATCH_SERVICE_PORT/volume/v3/\$(project_id)s" \
+        "$MIXMATCH_SERVICE_PROTOCOL://$HOST_IP:$MIXMATCH_SERVICE_PORT/volume/v3/\$(project_id)s"
+}
+
+register_mixmatch
+
 exit 0
