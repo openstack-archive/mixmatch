2dfed53be0993699972b791a17b5acccb45cf544
Eric Juma <ericjuma@gmail.com>
2016-12-20 18:37:38 -0500

    New samples for tests
    Add JSON sample files in the new samples directory. Use these
    sample files in the samples module. Refactor tests to use new
    samples.
    
    Change-Id: I414c62ddbe5ed3fc7c4d9474a4dad5142af2d51b

--- mixmatch/tests/unit/samples.py
+++ mixmatch/tests/unit/samples.py
@@ -12,335 +12,40 @@
 #   License for the specific language governing permissions and limitations
 #   under the License.
 
-IMAGE_LIST_V2 = {
-    "images": [
-        {
-            "checksum": "eb9139e4942121f22bbc2afc0400b2a4",
-            "container_format": "ami",
-            "created_at": "2016-08-19T15:47:10Z",
-            "disk_format": "ami",
-            "file": "/v2/images/61f655c0-4511-4307-a257-4162c87a5130/file",
-            "id": "61f655c0-4511-4307-a257-4162c87a5130",
-            "kernel_id": "130b7b71-487a-4553-b336-0a72ec590c99",
-            "min_disk": 0,
-            "min_ram": 0,
-            "name": "cirros-0.3.4-x86_64-uec",
-            "owner": "5f4358e168b747a487fe34e64c5619b2",
-            "protected": False,
-            "ramdisk_id": "941882c5-b992-4fa9-bcba-9d25d2f4e3b8",
-            "schema": "/v2/schemas/image",
-            "self": "/v2/images/61f655c0-4511-4307-a257-4162c87a5130",
-            "size": 25165824,
-            "status": "active",
-            "tags": [],
-            "updated_at": "2016-08-19T15:47:10Z",
-            "virtual_size": None,
-            "visibility": "public"
-        },
-        {
-            "checksum": "be575a2b939972276ef675752936977f",
-            "container_format": "ari",
-            "created_at": "2016-08-19T15:47:08Z",
-            "disk_format": "ari",
-            "file": "/v2/images/941882c5-b992-4fa9-bcba-9d25d2f4e3b8/file",
-            "id": "941882c5-b992-4fa9-bcba-9d25d2f4e3b8",
-            "min_disk": 0,
-            "min_ram": 0,
-            "name": "cirros-0.3.4-x86_64-uec-ramdisk",
-            "owner": "5f4358e168b747a487fe34e64c5619b2",
-            "protected": False,
-            "schema": "/v2/schemas/image",
-            "self": "/v2/images/941882c5-b992-4fa9-bcba-9d25d2f4e3b8",
-            "size": 3740163,
-            "status": "active",
-            "tags": [],
-            "updated_at": "2016-08-19T15:47:08Z",
-            "virtual_size": None,
-            "visibility": "public"
-        },
-        {
-            "checksum": "8a40c862b5735975d82605c1dd395796",
-            "container_format": "aki",
-            "created_at": "2016-08-19T15:46:58Z",
-            "disk_format": "aki",
-            "file": "/v2/images/130b7b71-487a-4553-b336-0a72ec590c99/file",
-            "id": "130b7b71-487a-4553-b336-0a72ec590c99",
-            "min_disk": 0,
-            "min_ram": 0,
-            "name": "cirros-0.3.4-x86_64-uec-kernel",
-            "owner": "5f4358e168b747a487fe34e64c5619b2",
-            "protected": False,
-            "schema": "/v2/schemas/image",
-            "self": "/v2/images/130b7b71-487a-4553-b336-0a72ec590c99",
-            "size": 4979632,
-            "status": "active",
-            "tags": [],
-            "updated_at": "2016-08-19T15:47:02Z",
-            "virtual_size": None,
-            "visibility": "public"
-        }
-    ]
+import json
+import os
+
+
+filenames = [
+    'volume_list_v1',
+    'volume_list_v2',
+    'volume_list_paginated_v2',
+    'volume_list_detail_v1',
+    'volume_list_detail_v2',
+    'split_volume_list_detail_v2',
+    'image_list_v1',
+    'image_list_v2',
+    'split_image_list_v2',
+    'image_list_paginated_v2',
+]
+
+data_path = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'sample_data', '%s.json'
+)
+sample_data = {s: json.loads(open(data_path % s).read()) for s in filenames}
+
+two_response = {
+    '/image/v2/images': sample_data['split_image_list_v2'],
+    '/volume/v2/id/volumes/detail': sample_data['split_volume_list_detail_v2']
 }
 
-IMAGE_LIST_V2_2 = {
-    "images": [
-        {
-            "status": "active",
-            "name": "cirros-0.3.2-x86_64-disk",
-            "tags": [],
-            "container_format": "bare",
-            "created_at": "2014-11-07T17:07:06Z",
-            "disk_format": "qcow2",
-            "updated_at": "2014-11-07T17:19:09Z",
-            "visibility": "public",
-            "self": "/v2/images/1bea47ed-f6a9-463b-b423-14b9cca9ad27",
-            "min_disk": 0,
-            "protected": False,
-            "id": "1bea47ed-f6a9-463b-b423-14b9cca9ad27",
-            "file": "/v2/images/1bea47ed-f6a9-463b-b423-14b9cca9ad27/file",
-            "checksum": "64d7c1cd2b6f60c92c14662941cb7913",
-            "owner": "5ef70662f8b34079a6eddb8da9d75fe8",
-            "size": 13167616,
-            "min_ram": 0,
-            "schema": "/v2/schemas/image",
-            "virtual_size": None
-        },
-        {
-            "status": "active",
-            "name": "F17-x86_64-cfntools",
-            "tags": [],
-            "container_format": "bare",
-            "created_at": "2014-10-30T08:23:39Z",
-            "disk_format": "qcow2",
-            "updated_at": "2014-11-03T16:40:10Z",
-            "visibility": "public",
-            "self": "/v2/images/781b3762-9469-4cec-b58d-3349e5de4e9c",
-            "min_disk": 0,
-            "protected": False,
-            "id": "781b3762-9469-4cec-b58d-3349e5de4e9c",
-            "file": "/v2/images/781b3762-9469-4cec-b58d-3349e5de4e9c/file",
-            "checksum": "afab0f79bac770d61d24b4d0560b5f70",
-            "owner": "5ef70662f8b34079a6eddb8da9d75fe8",
-            "size": 476704768,
-            "min_ram": 0,
-            "schema": "/v2/schemas/image",
-            "virtual_size": None
-        }
-    ]
-}
-
-VOLUME_LIST_V2 = {
-    "volumes": [
-        {
-            "id": "69baebf2-c242-47f4-b0a3-ab1761cfe755",
-            "links": [
-                {
-                    "href": "http://localhost:8776/v2/"
-                            "5f4358e168b747a487fe34e64c5619b2/"
-                            "volumes/"
-                            "69baebf2-c242-47f4-b0a3-ab1761cfe755",
-                    "rel": "self"
-                },
-                {
-                    "href": "http://localhost:8776/"
-                            "5f4358e168b747a487fe34e64c5619b2/"
-                            "volumes/"
-                            "69baebf2-c242-47f4-b0a3-ab1761cfe755",
-                    "rel": "bookmark"
-                }
-            ],
-            "name": "volume1"
-        }
-    ]
-}
-
-VOLUME_LIST_V2_2 = {
-    "volumes": [
-        {
-            "id": "79baebf2-c242-47f4-b0a3-ab1761cfe755",
-            "links": [
-                {
-                    "href": "http://localhost:8776/v2/"
-                            "5f4358e168b747a487fe34e64c5619b2/"
-                            "volumes/"
-                            "79baebf2-c242-47f4-b0a3-ab1761cfe755",
-                    "rel": "self"
-                },
-                {
-                    "href": "http://localhost:8776/"
-                            "5f4358e168b747a487fe34e64c5619b2/"
-                            "volumes/"
-                            "79baebf2-c242-47f4-b0a3-ab1761cfe755",
-                    "rel": "bookmark"
-                }
-            ],
-            "name": "volume2"
-        }
-    ]
-}
-
-VOLUME_DETAILED_V2 = {
-    'volumes': [
-        {
-            "volume_type": "lvmdriver-1",
-            "created_at": "2016-12-08T19:47:37.000000",
-            "bootable": "False",
-            "name": "vol1",
-            "os-vol-mig-status-attr:name_id": None,
-            "consistencygroup_id": None,
-            "source_volid": None,
-            "multiattach": False,
-            "description": None,
-            "replication_status": "disabled",
-            "updated_at": "2016-12-08T19:47:39.000000",
-            "encrypted": False,
-            "os-vol-host-attr:host": "dragonfruit-idp@lvmdriver-1#lvmdriver-1",
-            "availability_zone": "nova",
-            "links": [
-                {
-                    "rel": "self",
-                    "href": "http://localhost:8776/v2/79579219f37b481aa3aad197\
-                    76f843bc/volumes/a8da5976-f8ff-4a13-8e92-24f3cf7ac06d"
-                },
-                {
-                    "rel": "bookmark",
-                    "href": "http://localhost:8776/79579219f37b481aa3aad19776f\
-                    843bc/volumes/a8da5976-f8ff-4a13-8e92-24f3cf7ac06d"
-                }
-            ],
-            "attachments": [],
-            "migration_status": None,
-            "snapshot_id": None,
-            "id": "a8da5976-f8ff-4a13-8e92-24f3cf7ac06d",
-            "size": 1,
-            "user_id": "5ef3fad4df274b61b4f16f816616208d",
-            "os-vol-tenant-attr:tenant_id": "79579219f37b481aa3aad19776f843bc",
-            "os-vol-mig-status-attr:migstat": None,
-            "metadata": {},
-            "status": "available"
-        }
-    ]
-}
-
-VOLUME_DETAILED_V2_2 = {
-    "volumes": [
-        {
-            "volume_type": "lvmdriver-1",
-            "created_at": "2016-12-08T19:47:54.000000",
-            "bootable": "False",
-            "name": "vol2",
-            "os-vol-mig-status-attr:name_id": None,
-            "consistencygroup_id": None,
-            "source_volid": None,
-            "multiattach": False,
-            "description": None,
-            "replication_status": "disabled",
-            "updated_at": "2016-12-08T19:47:55.000000",
-            "encrypted": False,
-            "os-vol-host-attr:host": "dragonfruit-idp@lvmdriver-1#lvmdriver-1",
-            "availability_zone": "nova",
-            "links": [
-                {
-                    "rel": "self",
-                    "href": "http://localhost:8776/v2/79579219f37b481aa3aad197\
-                    76f843bc/volumes/0f1cef9e-cea0-4237-be35-5873670aa771"
-                },
-                {
-                    "rel": "bookmark",
-                    "href": "http://localhost:8776/79579219f37b481aa3aad19776f\
-                    843bc/volumes/0f1cef9e-cea0-4237-be35-5873670aa771"
-                }
-            ],
-            "attachments": [],
-            "migration_status": None,
-            "snapshot_id": None,
-            "id": "0f1cef9e-cea0-4237-be35-5873670aa771",
-            "size": 2,
-            "user_id": "5ef3fad4df274b61b4f16f816616208d",
-            "os-vol-tenant-attr:tenant_id": "79579219f37b481aa3aad19776f843bc",
-            "os-vol-mig-status-attr:migstat": None,
-            "metadata": {},
-            "status": "available"
-        }
-    ]
-}
-
-VOLUME_DETAILED_V2 = {
-    'volumes': [
-        {
-            "volume_type": "lvmdriver-1",
-            "created_at": "2016-12-08T19:47:37.000000",
-            "bootable": "False",
-            "name": "vol1",
-            "os-vol-mig-status-attr:name_id": None,
-            "consistencygroup_id": None,
-            "source_volid": None,
-            "multiattach": False,
-            "description": None,
-            "replication_status": "disabled",
-            "updated_at": "2016-12-08T19:47:39.000000",
-            "encrypted": False,
-            "os-vol-host-attr:host": "dragonfruit-idp@lvmdriver-1#lvmdriver-1",
-            "availability_zone": "nova",
-            "links": [
-                {
-                    "rel": "self",
-                    "href": "http://localhost:8776/v2/79579219f37b481aa3aad197\
-                    76f843bc/volumes/a8da5976-f8ff-4a13-8e92-24f3cf7ac06d"
-                },
-                {
-                    "rel": "bookmark",
-                    "href": "http://localhost:8776/79579219f37b481aa3aad19776f\
-                    843bc/volumes/a8da5976-f8ff-4a13-8e92-24f3cf7ac06d"
-                }
-            ],
-            "attachments": [],
-            "migration_status": None,
-            "snapshot_id": None,
-            "id": "a8da5976-f8ff-4a13-8e92-24f3cf7ac06d",
-            "size": 1,
-            "user_id": "5ef3fad4df274b61b4f16f816616208d",
-            "os-vol-tenant-attr:tenant_id": "79579219f37b481aa3aad19776f843bc",
-            "os-vol-mig-status-attr:migstat": None,
-            "metadata": {},
-            "status": "available"
-        }
-    ]
-}
-
-VOLUME_DETAILED_V2_COMBINED = {
-    "volumes": [
-        {
-            "name": "vol2",
-            "links": [
-                {
-                    "rel": "self",
-                    "href": "http://localhost:8776/v2/79579219f37b481aa3aad197\
-                    76f843bc/volumes/0f1cef9e-cea0-4237-be35-5873670aa771"
-                },
-                {
-                    "rel": "bookmark",
-                    "href": "http://localhost:8776/79579219f37b481aa3aad19776f\
-                    843bc/volumes/0f1cef9e-cea0-4237-be35-5873670aa771"
-                }
-            ],
-            "id": "0f1cef9e-cea0-4237-be35-5873670aa771",
-        },
-        {
-            "name": "vol1",
-            "links": [
-                {
-                    "rel": "self",
-                    "href": "http://localhost:8776/v2/79579219f37b481aa3aad197\
-                    76f843bc/volumes/a8da5976-f8ff-4a13-8e92-24f3cf7ac06d"
-                },
-                {
-                    "rel": "bookmark",
-                    "href": "http://localhost:8776/79579219f37b481aa3aad19776f\
-                    843bc/volumes/a8da5976-f8ff-4a13-8e92-24f3cf7ac06d"
-                }
-            ],
-            "id": "a8da5976-f8ff-4a13-8e92-24f3cf7ac06d",
-        }
-    ]
+one_response = {
+    '/image/v1/images': sample_data['image_list_v1'],
+    '/image/v2/images': sample_data['image_list_v2'],
+    '/image/v2/images?limit=1': sample_data['image_list_paginated_v2'],
+    '/volume/v1/id/volumes': sample_data['volume_list_v1'],
+    '/volume/v1/id/volumes/detail': sample_data['volume_list_detail_v1'],
+    '/volume/v2/id/volumes': sample_data['volume_list_v2'],
+    '/volume/v2/id/volumes/detail': sample_data['volume_list_detail_v2'],
+    '/volume/v2/id/volumes?limit=1': sample_data['volume_list_paginated_v2']
 }
