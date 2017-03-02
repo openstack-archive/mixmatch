d17b0645c5e1ba87fea9677b31d66394dab13073
Eric Juma <ericjuma@gmail.com>
2017-03-02 15:33:41 -0500

    Fix UWSGI logging to stderr
    All of uwsgi's logs were going to stderr, while the python logs
    were going to stdout. This caused redirection to be unintuitive
    by only redirecting the python logs. This change makes all output
    stdout instead of it being split half and half with stderr even
    though there were no errors.
    
    Change-Id: I825dee6f57eebec154b6a57a4450442bf9151978

--- run_proxy.sh
+++ run_proxy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+stdbuf -i0 \
 uwsgi --socket 0.0.0.0:5001 \
     --protocol=http \
     --http-chunked-input \
     -w mixmatch.wsgi \
     --master \
-    --processes 4 \
-    --threads 2
+    --processes 8 \
+    --threads 1 \
+    2>&1
