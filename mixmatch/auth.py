dce26c6ec1b0ab19ca9f0c78bc30c04c8c9adc95
Leo Tomatsu <ltomatsu@bu.edu>
2017-08-04 19:03:41 +0000

    Endpoint discovery
    If the service url is not provided in the configuration file, it
    uses the session object, it stores the service url in its cache.
    The service url for the default service provider is necessary.
    
    Change-Id: Ifeb2f8f350827582aff04c3bc56c146c8ce3afa5

--- mixmatch/auth.py
+++ mixmatch/auth.py
@@ -18,10 +18,18 @@ from keystoneclient import v3
 from keystoneauth1.exceptions import http
 
 import json
+import os
+
+try:
+    from urllib.parse import urlparse, urlunparse
+except ImportError:
+    from urlparse import urlparse, urlunparse
+
 
 from flask import abort
 
 from mixmatch import config
+from mixmatch import utils
 
 CONF = config.CONF
 LOG = config.LOG
@@ -30,9 +38,9 @@ MEMOIZE_SESSION = config.auth.MEMOIZE
 
 
 @MEMOIZE_SESSION
-def get_client():
-    """Return a Keystone client capable of validating tokens."""
-    LOG.info("Getting Admin Client")
+def get_admin_session():
+    """Return a Keystone session using admin service credentials."""
+    LOG.info("Getting Admin Session")
     service_auth = identity.Password(
         auth_url=CONF.auth.auth_url,
         username=CONF.auth.username,
@@ -42,14 +50,24 @@ def get_client():
         user_domain_id=CONF.auth.user_domain_id
     )
     local_session = session.Session(auth=service_auth)
-    return v3.client.Client(session=local_session)
+
+    return local_session
+
+
+@MEMOIZE_SESSION
+def get_client(session):
+    """Return a client object given a session object."""
+    LOG.debug("Getting client for %s" % session)
+
+    return v3.client.Client(session=session)
 
 
 @MEMOIZE_SESSION
 def get_local_auth(user_token):
     """Return a Keystone session for the local cluster."""
     LOG.debug("Getting session for %s" % user_token)
-    client = get_client()
+    admin_session = get_admin_session()
+    client = get_client(admin_session)
     token = v3.tokens.TokenManager(client)
 
     try:
@@ -62,7 +80,6 @@ def get_local_auth(user_token):
     local_auth = identity.v3.Token(auth_url=CONF.auth.auth_url,
                                    token=user_token,
                                    project_id=project_id)
-
     return session.Session(auth=local_auth)
 
 
@@ -106,3 +123,62 @@ def get_sp_auth(service_provider, user_token, remote_project_id):
     )
 
     return session.Session(auth=remote_auth)
+
+
+def _trim_endpoint(endpoint):
+    """Removes the project_id and version from the endpoint."""
+    endpoint = list(urlparse(endpoint))  # type: urlparse.ParseResult
+    # Note(knikolla): Elements are in order 'scheme, netloc, path'
+    path = endpoint[2].split('/')
+    utils.pop_tail_if_uuid(path)
+    # FIXME(knikolla): Write proper regex
+    if utils.safe_get(path, -1) in ['v1', 'v2', 'v3']:
+        path.pop(-1)
+    endpoint[2] = os.path.join(*path)
+
+    return urlunparse(endpoint)
+
+
+def _get_conf_endpoint(sp, service):
+    """Return an endpoint for a service in conf."""
+    conf = config.service_providers.get(CONF, sp)
+    endpoint = None
+
+    if service == 'image':
+        endpoint = conf.image_endpoint
+    elif service == 'volume':
+        endpoint = conf.volume_endpoint
+    elif service == 'network':
+        endpoint = conf.network_endpoint
+
+    return endpoint
+
+
+@MEMOIZE_SESSION
+def get_sp_endpoint(sp, service, version):
+    """Return an endpoint for a service."""
+    endpoint = _get_conf_endpoint(sp, service)
+
+    # If endpoint is not in conf, it will try to discover it
+    if endpoint is None:
+        if sp == 'default':
+            LOG.error('No service url configured for (%s, %s)'
+                      % (sp, service))
+            raise
+        else:
+            admin_session = get_admin_session()
+            token = admin_session.get_token()
+            project_id = get_projects_at_sp(sp,
+                                            token)[0]
+            auth_session = get_sp_auth(sp, token, project_id)
+            endpoint = auth_session.get_endpoint(service_type=service,
+                                                 interface='publicURL',
+                                                 min_version=version,
+                                                 max_version=version)
+            if endpoint is None:
+                LOG.error('No service url configured for (%s, %s)'
+                          % (sp, service))
+                raise
+
+    endpoint = _trim_endpoint(endpoint)
+    return endpoint
---
+++ mixmatch/tests/unit/test_trim_endpoint.py
@@ -0,0 +1,98 @@
+#   Copyright 2016 Massachusetts Open Cloud
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+import uuid
+
+from testtools import testcase
+from mixmatch.auth import _trim_endpoint
+
+
+class TrimEndpoint(testcase.TestCase):
+    def setUp(self):
+        super(TrimEndpoint, self).setUp()
+
+    def _construct_url(self, ip_address, service,
+                       version=None, project_id=None):
+        url = '%s/%s' % (ip_address, service)
+        if version:
+            url = '%s/%s' % (url, version)
+        if project_id:
+            url = '%s/%s' % (url, project_id)
+
+        return url
+
+    def test_trim_volumev1(self):
+        ip_address = 'http://jiji'
+        service = 'volume'
+        version = 'v1'
+        project_id = uuid.uuid4().hex
+
+        sample_url = self._construct_url(ip_address, service,
+                                         version, project_id)
+        test_url = _trim_endpoint(sample_url)
+        actual_url = self._construct_url(ip_address, service)
+
+        self.assertEqual(test_url, actual_url)
+
+    def test_trim_volumev2(self):
+        ip_address = 'http://kiki'
+        service = 'volume'
+        version = 'v2'
+        project_id = uuid.uuid4().hex
+
+        sample_url = self._construct_url(ip_address, service,
+                                         version, project_id)
+        test_url = _trim_endpoint(sample_url)
+        actual_url = self._construct_url(ip_address, service)
+
+        self.assertEqual(test_url, actual_url)
+
+    def test_trim_volumev3(self):
+        ip_address = 'http://tombo'
+        service = 'volume'
+        version = 'v3'
+        project_id = uuid.uuid4().hex
+
+        sample_url = self._construct_url(ip_address, service,
+                                         version, project_id)
+        test_url = _trim_endpoint(sample_url)
+        actual_url = self._construct_url(ip_address, service)
+
+        self.assertEqual(test_url, actual_url)
+
+    def test_trim_image(self):
+        ip_address = 'http://ursula'
+        service = 'image'
+        version = 'v2'
+        project_id = None
+
+        sample_url = self._construct_url(ip_address, service,
+                                         version, project_id)
+        test_url = _trim_endpoint(sample_url)
+        actual_url = self._construct_url(ip_address, service)
+
+        self.assertEqual(test_url, actual_url)
+
+    def test_network(self):
+        ip_address = 'http://osono'
+        service = 'network'
+        version = 'v2'
+        project_id = None
+
+        sample_url = self._construct_url(ip_address, service,
+                                         version, project_id)
+        test_url = _trim_endpoint(sample_url)
+        actual_url = self._construct_url(ip_address, service)
+
+        self.assertEqual(test_url, actual_url)
