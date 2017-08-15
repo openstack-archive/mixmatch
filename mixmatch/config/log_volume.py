1323dae062e845768605f3c451d84109fa04e8b9
ltomatsu <ltomatsu@bu.edu>
2017-08-15 14:08:03 +0000

    Multiple Log files
    Multiple loggers are created with their own
    configuration and logged corresponding to their
    service request. Still needs output configuration.
    
    Change-Id: I0083eed093436f16aa28400a6f1652bbff6ff579

---
+++ mixmatch/config/log_volume.py
@@ -0,0 +1,30 @@
+#   Copyright 2017 Massachusetts Open Cloud
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+from oslo_log import log
+# NOTE(ltomatsu): needs way to configure each logger
+
+LOG_volume = log.getLogger('volume')
+
+GROUP = None
+
+OPTS = []
+
+
+def pre_config(CONF):
+    pass
+
+
+def post_config(CONF):
+    pass
