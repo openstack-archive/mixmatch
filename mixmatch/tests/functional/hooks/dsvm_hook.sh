661d605a435518c7a6a7b0e8b4668ebc67d5f630
Kristi Nikolla <kristi@nikolla.me>
2018-05-24 10:01:39 -0700

    WIP - Remove volume v1 from devstack plugin and add block-storage
    With volume v1 being removed from cinder, we need to stop registering
    the endpoint. Also it looks like devstack registers a block-storage
    endpoint which points to volume v3.
    
    WIP: Currently testing only a subset.
    
    Change-Id: I565e9d9c61436bc0ce4651f7c7029e3e0dbdf3ac

--- mixmatch/tests/functional/hooks/dsvm_hook.sh
+++ mixmatch/tests/functional/hooks/dsvm_hook.sh
@@ -38,5 +38,5 @@ fi
 sudo chown -R $USER:stack $BASE/new/tempest
 sudo chown -R $USER:stack /opt/stack/data/tempest
 
-ostestr -r '(^tempest.api.compute|^tempest.api.image|^tempest.api.volume|^tempest.api.network|^tempest.scenario)' \
+ostestr -r '(^tempest.api.compute|^tempest.api.volume|^tempest.scenario)' \
     --blacklist-file $BASE/new/mixmatch/mixmatch/tests/functional/tempest_blacklist.txt
